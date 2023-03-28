93b7a70316a52f47456c62a93558ff7fe41a42a8
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-03-28 12:24:02 +0530

    some changes in social Icon + sun.jpg changed

--- src/NavBar/SocialIcon.jsx
+++ src/NavBar/SocialIcon.jsx
@@ -13,17 +13,17 @@ export default function SocialIcon() {
         <div>
             <Flex height="100vh" flexDirection={"column"} justifyContent="center" position={"fixed"} px='4' zIndex={'1'}>
                 <Flex flexDirection={"column"} alignItems="center" gap='10px' left='0'>
-                    <Box height={'100px'} w="2px" bg={color}></Box>
+                    <Box height={'100px'} w="2px" bg={bg}></Box>
                     <NavLink to='https://github.com/code735' style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
-                        <BsGithub color={color} />
+                        <BsGithub color={bg} />
                     </NavLink>
                     <NavLink to="https://www.linkedin.com/in/prathmesh-kumbhar-54287b1a3" style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
-                        <ImLinkedin2 color={color} />
+                        <ImLinkedin2 color={bg} />
                     </NavLink>
                     <NavLink to="https://app.slack.com/client/T03EXRYG70E/C04UZN7JT4H/rimeto_profile/U04P7EJ4JNP" style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
-                        <BsSlack color={color} />
+                        <BsSlack color={bg} />
                     </NavLink>
-                    <Box height={'100px'} w="2px" bg={color}></Box>
+                    <Box height={'100px'} w="2px" bg={bg}></Box>
                 </Flex>
             </Flex>
         </div >
