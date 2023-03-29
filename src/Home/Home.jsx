6ea4494be6abe77b019c75bc66ad6e038c1c8fdb
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-03-29 09:52:12 +0530

    Home section Modification

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,28 +4,30 @@ import { useColorModeValue } from '@chakra-ui/react'
 
 export default function Home() {
     var bg = useColorModeValue("wheat", "#1A202C")
+    var color = useColorModeValue("white", "black")
+
     return (
         <Box
             margin={'auto'}
             flexDirection={"column"}
-            height={'90vh'}
+            height={['100vh']}
             display="flex"
             justifyContent={"center"}
             alignItems={"center"}
             id="home"
             bg={'transparent'}
-            border={[`35px solid ${bg}`, `40px solid ${bg}`]}
+            border={[`20px solid ${bg}`, `30px solid ${bg}`]}
         >
             <HStack justifyContent={"center"} alignItems={"center"} w="100%">
-                <Heading as='h1' color={bg} fontSize={["1.5rem", "2rem", "3rem"]}>
+                <Heading as='h1' color={color} fontSize={["1.5rem", "2rem", "3rem"]}>
                     Prathmesh
                 </Heading>
-                <Heading color={"#11468F"} fontSize={["1.5rem", "2rem", "3rem"]}>
+                <Heading color={color} fontSize={["1.5rem", "2rem", "3rem"]}>
                     Kumbhar
                 </Heading>
             </HStack>
             <HStack>
-                <Heading as='h1' fontSize={"1rem"}>
+                <Heading as='h1' fontSize={"1rem"} color={color}>
                     Fullstack Developer
                 </Heading>
             </HStack>
--- src/NavBar/SocialIcon.jsx
+++ src/NavBar/SocialIcon.jsx
@@ -8,22 +8,23 @@ export default function SocialIcon() {
     const color = useColorModeValue("#11468F", "white")
     const iconColor = useColorModeValue("white", "black")
     const bg = useColorModeValue("black", "white")
+    const navbarbg = useColorModeValue("white", "black");
 
     return (
         <div>
-            <Flex height="100vh" flexDirection={"column"} justifyContent="center" position={"fixed"} px='1' zIndex={'1'}>
-                <Flex flexDirection={"column"} alignItems="center" gap='10px' left='0'>
-                    <Box height={'100px'} w="2px" bg={bg}></Box>
-                    <NavLink to='https://github.com/code735' style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
+            <Flex justifyContent="center" position={"fixed"} px='1' bottom={['15px', '30px']} width={'100%'} py={'10px'} zIndex={'1'}>
+                <Flex alignItems="center" gap='10px' left='0'>
+
+                    <NavLink to='https://github.com/code735' style={{ borderRadius: "50%", padding: "10px", background: navbarbg }} target='_blank'>
                         <BsGithub color={bg} />
                     </NavLink>
-                    <NavLink to="https://www.linkedin.com/in/prathmesh-kumbhar-54287b1a3" style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
+                    <NavLink to="https://www.linkedin.com/in/prathmesh-kumbhar-54287b1a3" style={{ borderRadius: "50%", padding: "10px", background: navbarbg }} target='_blank'>
                         <ImLinkedin2 color={bg} />
                     </NavLink>
-                    <NavLink to="https://app.slack.com/client/T03EXRYG70E/C04UZN7JT4H/rimeto_profile/U04P7EJ4JNP" style={{ borderRadius: "50%", padding: "5px" }} target='_blank'>
+                    <NavLink to="https://app.slack.com/client/T03EXRYG70E/C04UZN7JT4H/rimeto_profile/U04P7EJ4JNP" style={{ borderRadius: "50%", padding: "10px", background: navbarbg }} target='_blank'>
                         <BsSlack color={bg} />
                     </NavLink>
-                    <Box height={'100px'} w="2px" bg={bg}></Box>
+
                 </Flex>
             </Flex>
         </div >
