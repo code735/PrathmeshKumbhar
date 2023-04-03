a561fbb797a4633cec8678142af326133cd9ad33
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-04-03 19:14:19 +0530

    Some Navbar changes

--- src/App.js
+++ src/App.js
@@ -18,67 +18,30 @@ function App() {
   const icon = useColorModeValue(<FiMoon fontSize="1.3rem" />, <CiSun fontSize="1.5rem" />)
   const buttonbg = useColorModeValue("white", "#060116");
   var dispatch = useDispatch();
-  var navbarbg = useSelector(state => state.navbarBg);
-
-  useEffect(() => {
-    console.log(navbarbg)
-  }, [navbarbg])
-
-  const [opacity, setOpacity] = useState(0);
-
-  useEffect(() => {
-    function handleScroll() {
-      const scrollTop = window.pageYOffset;
-      if (scrollTop >= 10) {
-        setOpacity(1);
-      } else {
-        setOpacity(0);
-      }
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <div className="App">
       <Box position={'fixed'} w={['100%']} margin={'auto'} py={'2'} zIndex='1' id='myDiv'>
         <motion.div>
           <Flex alignItems="center" justifyContent="space-between" bg={'transparent'} gap="10px" margin="0 auto" padding='5px 30px'>
-            <Box width={'20px'} display={["none", "", "", "block"]}>
-              <Image src={logo}  ></Image>
-            </Box>
-            <Nav />
-            <Button onClick={() => {
-              toggleColorMode();
-              dispatch(PRELOADER_TOGGLE_FUNCTION(true));
-            }} display={["", "", "", "none"]} px="2" bg={buttonbg}>
-              {icon}
+            <Button display={["none", "", "", "block"]} bg={'black'} p={'2'}>
+              <Image src={logo} ></Image>
             </Button>
-            <MobileMenu />
-          </Flex>
-        </motion.div>
-      </Box>
-      <Box position={'fixed'} w={['100%']} margin={'auto'} py={'2'} zIndex='2' id='myDiv' style={{
-        background: "black",
-        opacity: opacity,
-        transition: 'opacity 1s ease-in-out'
-      }}>
-        <motion.div>
-          <Flex alignItems="center" justifyContent="space-between" bg={'transparent'} gap="10px" margin="0 auto" padding='5px 30px'>
-            <Box width={'20px'} display={["none", "", "", "block"]}>
-              <Image src={logo}  ></Image>
-            </Box>
-            <Nav />
-            <Button onClick={() => {
-              toggleColorMode();
-              dispatch(PRELOADER_TOGGLE_FUNCTION(true));
-            }} display={["", "", "", "none"]} px="2" bg={buttonbg}>
-              {icon}
-            </Button>
+            <Flex
+              gap={'20px'}
+            >
+              <Button onClick={() => {
+                toggleColorMode();
+                dispatch(PRELOADER_TOGGLE_FUNCTION(true));
+              }}
+                px="2"
+                bg={'black'}
+                color={'white'}
+              >
+                {icon}
+              </Button>
+              <Nav />
+            </Flex>
             <MobileMenu />
           </Flex>
         </motion.div>
@@ -87,7 +50,7 @@ function App() {
         <SocialIcon />
       </Box>
       <Container />
-    </div>
+    </div >
   );
 }
 
--- src/NavBar/Nav.jsx
+++ src/NavBar/Nav.jsx
@@ -1,10 +1,22 @@
-import { Button, Flex, useColorMode, useColorModeValue, Tooltip } from '@chakra-ui/react'
+import {
+    Button,
+    Flex,
+    useColorMode,
+    useColorModeValue,
+    Drawer,
+    DrawerOverlay,
+    DrawerCloseButton,
+    DrawerBody,
+    DrawerContent,
+    Box,
+    IconButton
+} from '@chakra-ui/react'
 import React from 'react'
 import { FiMoon } from 'react-icons/fi'
 import { CiSun } from 'react-icons/ci'
 import { useSelector, useDispatch } from 'react-redux'
-import { PRELOADER_TOGGLE_FUNCTION } from '../Redux/action'
-import { motion } from 'framer-motion'
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons"
+import { useDisclosure } from '@chakra-ui/react'
 
 export default function Nav() {
     const { toggleColorMode } = useColorMode();
@@ -14,34 +26,82 @@ export default function Nav() {
     var togglepreloader = useSelector(state => state.togglepreloader);
     var dispatch = useDispatch();
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+
     return (
-        <Flex display={["none", "", "", "flex"]} width="50%" justifyContent="space-between" alignItems="center">
-            <a href='#home'>
-                <Button px="2" bg={buttonbg} borderRadius='0' color={'white'}>
-                    Home
-                </Button>
-            </a>
-            <a href="#about">
-                <Button px="2" bg={buttonbg} borderRadius='0' color={'white'}>
-                    About
-                </Button>
-            </a>
-            <a href="#projects">
-                <Button px="2" bg={buttonbg} borderRadius='0' color={'white'}>
-                    Project
-                </Button>
-            </a>
-            <a href="#skills">
-                <Button px="2" bg={buttonbg} borderRadius='0' color={'white'}>
-                    Skills
-                </Button>
-            </a>
-            <Button onClick={() => {
-                toggleColorMode();
-                dispatch(PRELOADER_TOGGLE_FUNCTION(true));
-            }} px="2" bg={buttonbg} borderRadius='0' color={'white'}>
-                {icon}
-            </Button>
-        </Flex >
+        <Box
+            display={['none', '', '', 'block']}
+        >
+            <Button ref={btnRef} as={IconButton} onClick={onOpen} icon={<HamburgerIcon />} color={'white'} bg={'black'} />
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent bg={'gray.700'}>
+                    <DrawerCloseButton color={'white'} />
+
+                    <DrawerBody
+                        display={'flex'}
+                        justifyContent={'flex-start'}
+                        flexDirection={'column'}
+                        height={'100vh'}
+                    >
+                        <Flex
+                            display={["none", "", "", "flex"]}
+                            flexDir={'column'}
+                            alignItems="space-between"
+                            gap={'40px'}
+                            height={'100vh'}
+                        >
+                            <a href='#home'>
+                                <Button px="2"
+                                    bg={buttonbg}
+                                    borderRadius='0'
+                                    color={'white'}
+                                    fontSize={'2rem'}
+                                >
+                                    Home
+                                </Button>
+                            </a>
+                            <a href="#about">
+                                <Button px="2"
+                                    bg={buttonbg}
+                                    borderRadius='0'
+                                    color={'white'}
+                                    fontSize={'2rem'}
+                                >
+                                    About
+                                </Button>
+                            </a>
+                            <a href="#projects">
+                                <Button px="2"
+                                    bg={buttonbg}
+                                    borderRadius='0'
+                                    color={'white'}
+                                    fontSize={'2rem'}
+                                >
+                                    Project
+                                </Button>
+                            </a>
+                            <a href="#skills">
+                                <Button px="2"
+                                    bg={buttonbg}
+                                    borderRadius='0'
+                                    color={'white'}
+                                    fontSize={'2rem'}
+                                >
+                                    Skills
+                                </Button>
+                            </a>
+                            {/*  */}
+                        </Flex >
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </Box>
     )
 }
