9d054536b0ce86e4b4eaa686f652d053cc531013
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-04-13 14:01:32 +0530

    Projects section done

--- src/Preloader/Preloader.jsx
+++ src/Preloader/Preloader.jsx
@@ -10,7 +10,7 @@ export const Preloader = () => {
             if (percentage < 100) {
                 setPercentage((prevPercentage) => prevPercentage + 1);
             }
-        }, 50);
+        }, 10);
 
         return () => clearInterval(interval);
     }, [percentage]);
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -14,8 +14,8 @@ import zostel from '../images/zostel.jpg'
 import zoomcar from '../images/zoomcar.png'
 import stanzaliving from '../images/stanzaliving.png'
 import nykaa from '../images/nykaa.png'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { BsGithub } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import { FaGithub, FaLink } from 'react-icons/fa'
 
 export default function About() {
     var bg = useColorModeValue("wheat", "black")
@@ -23,33 +23,38 @@ export default function About() {
     var cardColor = useColorModeValue("#ffd483", "#2d3748")
     var btncolor = useColorModeValue("white", "grey")
     var textcolor = useColorModeValue("black", "white")
+    var navigate = useNavigate();
 
     var projects = [
         {
             name: 'Zostel',
-            img: zostel,
-            techstack: ['ReactJS', 'ChakraUI', 'AJAX', 'Axios', 'Redux', 'Google Map API'],
+            logo: zostel,
+            cardimg: "https://img.etimg.com/thumb/msid-72463946,width-1200,height-900,imgsize-1057355,resizemode-8,quality-100/prime/technology-and-startups/hostel-chain-pioneer-zostel-is-boxed-into-a-corner-explores-new-products-in-the-indian-travel-market.jpg",
+            desc: "Zostel is a network of hostels and homes in India. It has presence in 44 cities in India and Nepal",
             link: 'https://zostel-ten.vercel.app/',
             githubLink: 'https://github.com/code735/zostel'
         },
         {
             name: "Zoomcar",
-            img: zoomcar,
-            techstack: ['ReactJS', 'AJAX', 'Router', 'Context API', 'Google Maps API'],
+            logo: zoomcar,
+            cardimg: "https://officechai.com/wp-content/uploads/2016/09/l-zoomcar-2.jpg",
+            desc: "Zoomcar is India’s largest marketplace for cars on rent. From short road trips to quick in-city drives for groceries, supply pick-up, food runs, we have the cheapest car rental options for all your needs!",
             link: "https://zoomcar-indol.vercel.app/",
             githubLink: 'https://github.com/code735/zoomcar'
         },
         {
             name: "StanzaLiving",
-            img: stanzaliving,
-            techstack: ['Javascript', 'AJAX', 'Google Maps API', "Bootstrap"],
+            logo: stanzaliving,
+            cardimg: "https://images.livemint.com/img/2021/03/12/1600x900/Stanza_Living_Office_(1)_1568191516604_1615563265333.jpg",
+            desc: "Stanza Living is the common brand name for Dtwelve Spaces Private Limited. It provides fully-managed shared living accommodations to students and young professionals.",
             link: "https://code735.github.io/stanzaLiving/",
             githubLink: 'https://github.com/code735/stanzaLiving'
         },
         {
             name: "Nykaa",
-            img: nykaa,
-            techstack: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
+            logo: nykaa,
+            cardimg: "https://media.glassdoor.com/lst2x/797724/fsn-ecommerce-ventures-office.jpg",
+            desc: "Nykaa is an Indian e-commerce company, founded by Falguni Nayar in 2012 and headquartered in Mumbai.",
             link: "https://code735.github.io/Nykaa_Clone/",
             githubLink: 'https://github.com/code735/Nykaa_Clone'
         },
@@ -78,118 +83,70 @@ export default function About() {
                 </Heading>
                 <Box borderBottom={'1px solid'} w={'150px'} margin={"auto"} mb={'2rem'}></Box>
                 <Grid
-                    templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']}
-                    gap={5}
+                    templateColumns={['repeat(2, 1fr)', 'repeat(4, 1fr)', 'repeat(4, 1fr)', '']}
+                    gap={2}
                     p={['1', '3', '4']}
                 >
-                    {
-                        projects.map((el) => {
-                            return <GridItem
+                    {projects.map((el) => {
+                        return (
+                            <GridItem
                                 w='100%'
-                                p={'5'}
-                                bg={cardColor}
+                                bgSize={'cover'}
+                                bgRepeat={'no-repeat'}
                                 alignItems={'center'}
+                                border={'1px solid grey'}
+                                borderRadius={'2'}
                             >
+                                <Image src={el.cardimg} objectFit={'cover'} h={["80%", '60%', '', "70%"]} />
                                 <Flex
                                     flexDir={'column'}
-                                    alignItems={'center'}
-                                    gap={'10px'}
-                                    h={'100%'}
-                                    justifyContent={'space-between'}
+                                    gap={['0px', '', '', '10px']}
+                                    p={['1', '', '', '2']}
                                 >
-                                    <Image
-                                        src={el.img}
-                                        objectFit={'cover'}
-                                        w={'60px'}
-                                        h={'60px'}
-                                        borderRadius={'100%'}
-                                        display={['none', '', '', '', 'block']}
-                                    ></Image>
-                                    <Heading
-                                        textAlign={'center'}
-                                        fontSize={['.8rem', '', '', '1rem', '1.5rem']}
+                                    <Text fontWeight={'bold'}>{el.name} Clone</Text>
+                                    <Text
+                                        display={['none', '', '-webkit-box', '']}
+                                        css={{ WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden' }}
                                         fontFamily={'EB Garamond, serif'}
-                                        fontWeight={'400'}
                                     >
-                                        {el.name} Clone
-                                    </Heading>
-                                    <Flex
-                                        flexWrap={'wrap'}
-                                        gap={'10px'}
-                                        justifyContent={'center'}
-                                        display={['none', '', '', '', 'flex']}
-                                    >
-                                        {
-                                            el.techstack.map((e) => {
-                                                return <Text>{e}</Text>
-                                            })
-                                        }
-                                    </Flex>
-                                    <Flex
+                                        {el.desc}
+                                    </Text>
+                                    <Box
+                                        display={['none', '', '', 'flex']}
                                         alignItems={'center'}
-                                        gap={'20px'}
+                                        gap={'10px'}
                                     >
-                                        <a
-                                            href={el.link}
-                                            target='_blank'
-                                        >
-                                            <Button
-                                                w={'100%'}
-                                                height={'35px'}
-                                                bg={'transparent'}
-                                                color={textcolor}
-                                                border={`1px solid ${textcolor}`}
-                                                display={'flex'}
-                                                alignItems={'center'}
-                                                gap={'10px'}
-                                                p={['0', '', '10px']}
-                                                borderRadius={'0'}
-                                            >
-                                                <Text
-                                                    display={['none', '', '', '', 'block']}
-                                                    fontWeight={'400'}
-                                                >Visit Site</Text>
-                                                <ExternalLinkIcon
-                                                    fontSize={['.8rem', '', '', '1rem']}
-                                                    fontWeight={'400'}
-                                                />
-                                            </Button>
-                                        </a>
-                                        <a
-                                            href={el.link}
-                                            target='_blank'
-                                        >
-                                            <Button
-                                                w={'100%'}
-                                                height={'35px'}
-                                                bg={'transparent'}
-                                                color={textcolor}
-                                                border={`1px solid ${textcolor}`}
-                                                display={'flex'}
-                                                alignItems={'center'}
-                                                gap={'10px'}
-                                                p={['0', '', '10px']}
-                                                borderRadius={'0'}
-                                                fontweight={'400'}
-                                            >
-                                                <Text p={'0'}
-                                                    display={['none', '', '', '', 'block']}
-                                                    fontWeight={'400'}
-                                                >Repo Link</Text>
-                                                <Text
-                                                    fontSize={['.8rem', '', '', '1rem']}
-                                                    fontWeight={'400'}
-                                                >
-                                                    <BsGithub />
-                                                </Text>
-                                            </Button>
-                                        </a>
-                                    </Flex>
+                                        <FaGithub
+                                            fontSize={'1.2rem'}
+                                            onClick={() => {
+                                                window.open(el.githubLink, "_blank")
+                                            }}
+                                            onMouseEnter={(e) => {
+                                                e.currentTarget.style.cursor = "pointer"
+                                            }}
+                                            onMouseLeave={(e) => {
+                                                e.currentTarget.style.cursor = "normal"
+                                            }}
+                                        />
+                                        <FaLink
+                                            fontSize={'1.2rem'}
+                                            onClick={() => {
+                                                window.open(el.link, "_blank")
+                                            }}
+                                            onMouseEnter={(e) => {
+                                                e.currentTarget.style.cursor = "pointer"
+                                            }}
+                                            onMouseLeave={(e) => {
+                                                e.currentTarget.style.cursor = "normal"
+                                            }}
+                                        />
+                                    </Box>
                                 </Flex>
                             </GridItem>
-                        })
-                    }
+                        );
+                    })}
                 </Grid>
+
             </Box>
         </Box >
     )
