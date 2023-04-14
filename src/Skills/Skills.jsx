177e123c2353615a0527ba2bd454749eab3f0f30
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-04-14 14:06:56 +0530

    skills section done

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,64 +2,127 @@ import {
     Box,
     Grid,
     GridItem,
+    Flex,
+    Heading,
+    Text,
     useColorModeValue
 } from '@chakra-ui/react'
 import React from 'react'
 import { GrReactjs } from 'react-icons/gr'
 import { FiFigma } from 'react-icons/fi'
-import { SiJavascript } from 'react-icons/si'
-import { ImCss3, ImHtmlFive } from 'react-icons/im'
+import { SiChakraui, SiRedux, SiReactrouter } from 'react-icons/si'
+import { TfiCss3 } from 'react-icons/tfi'
+import { RiHtml5Line } from 'react-icons/ri'
 import { FaJava } from 'react-icons/fa'
+import { IoLogoJavascript } from 'react-icons/io'
 
 export default function Skills() {
     var bg = useColorModeValue("wheat", "black")
     var data = [
         {
             icon: <GrReactjs />,
-            name: "ReactJS"
+            name: "ReactJS",
+            color: "black"
         },
         {
-            icon: <SiJavascript />,
-            name: "Javascript"
+            icon: <IoLogoJavascript />,
+            name: "Javascript",
+            color: "black"
         },
         {
-            icon: <ImHtmlFive />,
-            name: "HTML5"
+            icon: <RiHtml5Line />,
+            name: "HTML5",
+            color: "black"
         },
         {
-            icon: <ImCss3 />,
-            name: "CSS3"
+            icon: <TfiCss3 />,
+            name: "CSS3",
+            color: "black"
         },
         {
             icon: <FaJava />,
-            name: "Java"
+            name: "Java",
+            color: "black"
         },
         {
             icon: <FiFigma />,
-            name: "Figma"
+            name: "Figma",
+            color: "black"
+        },
+        {
+            icon: <SiChakraui />,
+            name: "ChakraUI",
+            color: "black"
+        },
+        {
+            icon: <SiReactrouter />,
+            name: "React Router",
+            color: "black"
+        },
+        {
+            icon: <SiRedux />,
+            name: "React Redux",
+            color: "black"
         }
     ]
 
     return (
-        <Box w={'100%'} margin={"auto"} bg={'#000000c4'} height={'100vh'} display={"flex"} flexDir={"column"} justifyContent={"center"} alignItems={"center"} id="skills">
-            <Grid
-                templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
-                gap={5}
+        <Box w={'100%'} margin={"auto"} height={'100vh'} display={"flex"} flexDir={"column"} justifyContent={"center"} alignItems={"center"} id="skills">
+            <Flex
+                flexDirection={['column', '', 'row']}
             >
-                {
-                    data.map((el) => {
-                        return <GridItem>
-                            <Box
-                                fontSize={'3rem'}
-                                p={'3'}
-                                border={'2px solid white'}
-                                borderRadius={'10px'}
-                                color={'white'}
-                            >{el.icon}</Box>
-                        </GridItem>
-                    })
-                }
-            </Grid>
-        </Box>
+                <Box
+                    bg={bg}
+                    w={['100%', '', "50%"]}
+                    h={'100vh'}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    flexDir={'column'}
+                    p={['2rem', '', '4rem']}
+                >
+                    <Heading
+                        fontFamily={'EB Garamond, serif'}
+                        fontWeight={'400'}
+                        borderBottom={'1px solid'}
+                        pb={'2'}
+                        mb={'3'}
+                        display={'block'}
+                        w={'100px'}
+                    >Skills</Heading>
+                    <Text
+                        fontFamily={'EB Garamond, serif'}
+                        fontWeight={'400'}
+                        fontSize={[".7rem", "", "", "", "1.2rem"]}
+                    >As a skilled web developer, I possess a strong proficiency in a variety of front-end web development technologies such as ReactJS, AJAX, HTML5, CSS, and Vanilla JavaScript. In addition, I have extensive experience working with UI frameworks including Chakra UI and Bootstrap, which allows me to create visually stunning and user-friendly web applications. My knowledge of state management tools such as Redux and routing frameworks like Router enables me to build complex and robust web applications. Additionally, I possess a solid foundation in back-end development with Java, which enables me to create comprehensive and functional web applications. With expertise in UI/UX design using Figma, I am well-equipped to create engaging user interfaces that enhance user experience. Overall, my diverse skill set and experience in web development enable me to create dynamic and polished web applications that exceed client expectations.</Text>
+                </Box>
+                <Box
+                    bg={'#ffffff0d'}
+                    margin={'1.5rem'}
+                    backdropFilter={'blur(10px)'}
+                    w={['100%', '', "50%"]}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    flexDir={'column'}
+                    alignItems={'center'}
+                >
+                    <Grid
+                        gap={'3rem'}
+                        templateColumns={'repeat(3,1fr)'}
+                    >
+                        {
+                            data.map((e) => {
+                                return <GridItem
+                                    color={e.color}
+                                    fontSize={'3rem'}
+                                    bg={'white'}
+                                    p={'20px'}
+                                    borderRadius={'7px'}
+                                > {e.icon}</GridItem>
+                            })
+                        }
+                    </Grid>
+                </Box>
+            </Flex >
+        </Box >
     )
 }
